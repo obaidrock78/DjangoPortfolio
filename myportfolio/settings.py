a5f69228646da888d8af772fef7a133fda53b414
obaidrock78 <obaidrock78@gmail.com>
2023-10-12 13:45:04 +0500

    buy_me_coffe

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
 
     "corsheaders.middleware.CorsMiddleware",
-
+    'django_cors.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     # "whitenoise.middleware.WhiteNoiseMiddleware",
 
